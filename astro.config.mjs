e577229ccb4daed82a72768db9f2e5da2264cbdc
yassine ben azouz <yassinebenazouz123@gmail.com>
2023-07-23 15:56:20 +0200

    preparing to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import partytown from "@astrojs/partytown";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), partytown()]
+  integrations: [react(), tailwind(), partytown()],
+  output: "server",
+  adapter: netlify()
 });
